49fddaf50ee9bbbb5fd62cc41edd8b9db6789d3d
Justin Knoepfler <knoe0023@umn.edu>
2014-11-21 21:33:56 -0600

    added camel-spark-rest support, upgraded to java 8

--- src/main/java/tweedar/jdbc/QueryTranslator.java
+++ src/main/java/tweedar/jdbc/QueryTranslator.java
@@ -7,5 +7,16 @@ public class QueryTranslator {
 		String query = "SELECT * FROM gridmap_test, bin_test WHERE gridmap_test.id = bin_test.gridmap_id && gridmap_test.id = 1415815712384;";
 		exchange.getOut().setBody(query); 
 	}
+	
+	public void getGridmaps(Exchange exchange){
+		String query = "SELECT * FROM gridmap";
+		exchange.getOut().setBody(query); 
+	}
+	
+	public void getGridmapId(Exchange exchange){
+		String gridmapId = (String)exchange.getIn().getHeader("gridmapId");
+		String query = "SELECT * FROM bin WHERE bin.gridmap_id = " + gridmapId;
+		exchange.getOut().setBody(query); 
+	}
 
 }
